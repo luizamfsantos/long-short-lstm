12786e7eb20f5b7534013e3990b548fb2cb05e8a
luizamfsantos <luiza.marques_@hotmail.com>
2024-10-11 19:43:24 -0300

    feat: zip raw data after ingestion

--- scripts/run_ingestion.sh
+++ scripts/run_ingestion.sh
@@ -9,5 +9,5 @@ end_date=$(date +%Y%m%d)
 # Run the Python script
 python -m ingestion.get_data -s $start_date -e $end_date --save_raw_data
 
-# Zip the raw data
-zip -r raw_data.zip raw_data
+# Zip the raw json files
+find test-data -type f -name "*.json" | zip -m raw_data.zip -@
