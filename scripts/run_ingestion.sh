7b65864cee63cf1343b5f2afdf979e9933c344bf
luizamfsantos <luiza.marques_@hotmail.com>
2024-10-11 19:13:53 -0300

    basic ingestion run

---
+++ scripts/run_ingestion.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set the start date
+start_date="20240101"
+
+# Get end date: today
+end_date=$(date +%Y%m%d)
+
+# Run the Python script
+python -m ingestion.get_data -s $start_date -e $end_date --save_raw_data
