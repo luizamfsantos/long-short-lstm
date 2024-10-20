576a048882de304c40dc29f4892fe5cc8dfc25bf
luizamfsantos <luiza.marques_@hotmail.com>
2024-10-20 09:15:36 -0300

    feat: get_logger, get_config, save model checkpoints to checkpoints folder

--- training/train.py
+++ training/train.py
@@ -1,15 +1,20 @@
+import torch
 from models.lstm_model import LSTMModel
 from lightning.pytorch import Trainer
-import torch
-import logging
+from training.train_utils import get_config, get_logger
+
 
 def main():
-    logger = logging.getLogger(__name__)
+    logger = get_logger()
     logger.info(f'Using torch {torch.__version__}')
-    lstm = LSTMModel()
-    trainer = Trainer(devices="auto", accelerator="auto")
-    train_loader = [] # TODO: add dataloader here
-    trainer.fit(model=lstm, train_dataloaders=train_loader)
+    config = get_config() # get hyperparameters from config.yaml
+    lstm = LSTMModel(config['INPUT_SIZE'], config['HIDDEN_SIZE'])
+    trainer = Trainer(
+        devices="auto",
+        accelerator="auto",
+        default_root_dir="checkpoints")
+    #train_loader = [] # TODO: add dataloader here
+    #trainer.fit(model=lstm, train_dataloaders=train_loader)
 
 if __name__ == '__main__':
     main()
---
+++ training/train_utils.py
@@ -0,0 +1,34 @@
+import yaml
+import logging
+import os
+
+def get_logger(logger_name='model_training'):
+    # Ensure the logs directory exists
+    os.makedirs('logs', exist_ok=True)
+    
+    # Create a custom logger
+    logger = logging.getLogger(logger_name)
+    
+    # Set the default logging level
+    logger.setLevel(logging.INFO)
+    
+    # Create handlers
+    c_handler = logging.StreamHandler()
+    f_handler = logging.FileHandler('logs/model_training.log')
+    
+    # Create formatters and add them to handlers
+    c_format = logging.Formatter('[%(asctime)s:%(levelname)s]: %(message)s')
+    f_format = logging.Formatter('[%(asctime)s:%(levelname)s]: %(message)s')
+    c_handler.setFormatter(c_format)
+    f_handler.setFormatter(f_format)
+    
+    # Add handlers to the logger
+    logger.addHandler(c_handler)
+    logger.addHandler(f_handler)
+    
+    return logger
+
+def get_config():
+    with open('config/config.yaml') as f:
+        config = yaml.safe_load(f)
+    return config['LSTM']
