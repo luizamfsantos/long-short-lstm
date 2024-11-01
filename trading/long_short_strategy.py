d9e0abe43c702f319c9f3ef87213371e09940d41
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-01 19:29:42 -0300

    add long short position in strategy

--- trading/long_short_strategy.py
+++ trading/long_short_strategy.py
@@ -1,37 +1,52 @@
 from simulator.strategy_interface import StrategyInterface
+import pandas as pd
 
 
 class LongShortStrategy(StrategyInterface):
-    def __init__(self):
-        pass
+    def __init__(self,
+                 long_count: int = 10,
+                 short_count: int = 10):
+        self.long_count = long_count
+        self.short_count = short_count
 
-    def calculate_next_weights(self, data, t, **kwargs):
+    def calculate_next_weights(
+            self,
+            forecast,
+            metadata,
+            t,
+            **kwargs):
         """
         Calculate the weights for the next period.
 
         Args:
-            data (dict): Dictionary containing necessary data.
+            forecast (torch.Tensor): predictions for each stock. (num_tickers, 1)
+                # Strategy will receive forecast from LSTM 
+                # near to 1 means buy, near to 0 means sell
+            metadata : information to retrieve the stock names 
             t (int): Time value for calculation.
 
         Returns:
             pd.DataFrame: Weights for the next period.
         """
-        #Get the data: model will receive forecast from LSTM
-        prices = data['stocks'] # TODO: adjust to use data from raw_combined parquet files
-        returns = prices.pct_change() # TODO: adjust to use column variacaopercent from data
-
-        # Use LSTM to predict the direction of each stock
-        # 
-
-        # # Calculate the momentum
-        # momentum = returns.rolling(window=252).mean().iloc[t - 1]
-
-        # # Select the top and bottom stocks
-        # top_stocks = momentum.head(10)
-        # bottom_stocks = momentum.tail(10)
-
-        # Calculate the weights: TODO: how to get the simulator know that bottom_stocks are short positions?
-        weights = pd.concat([top_stocks, bottom_stocks], axis=0)
-        weights = weights / weights.abs().sum()
-        # return a dataframe with the columns date, ticker, and weights
+        # Ensure that long_count and short_count are smaller than the number of stocks
+        forecast_length = forecast.shape[0]
+        if self.long_count > forecast_length or self.short_count > forecast_length:
+            long_count = forecast_length // 2  # top half
+            short_count = forecast_length - long_count  # bottom half
+        else:
+            long_count = self.long_count
+            short_count = self.short_count
+        # Select the top and bottom stocks
+        top_stocks_idx = forecast.topk(long_count).indices
+        top_stocks_names = ...  # TODO: get the names of the top stocks
+        bottom_stocks_idx = (-1*forecast).topk(short_count).indices
+        bottom_stocks_names = ...  # TODO: get the names of the top stocks
+        top_stocks = pd.DataFrame(
+            {'ticker': top_stocks_names, 'weights': 1, 'date': t, 'position': 'long'})
+        bottom_stocks = pd.DataFrame(
+            {'ticker': bottom_stocks_names, 'weights': 1, 'date': t, 'position': 'short'})
+        weights_df = pd.concat([top_stocks, bottom_stocks], axis=0)
+        weights_df['weights'] = weights_df['weights'] / \
+            weights_df['weights'].abs().sum()  # Normalize the weights
+        # return a dataframe with the columns date, ticker, weights and position
         return weights.to_frame().T
