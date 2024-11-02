264e57895806646e358a2b3d765c3853baac1e69
luizamfsantos <luiza.marques_@hotmail.com>
2024-11-02 07:31:17 -0300

    add data module to trainer

--- training/train.py
+++ training/train.py
@@ -6,7 +6,7 @@ from models.lstm_model import LSTMModel
 import torch.nn.functional as F
 from lightning.pytorch import Trainer
 from training.train_utils import get_config, get_logger
-from models.data_preparation import TimeSeriesData
+from models.data_preparation import TimeSeriesDataModule
 from torch.utils.data import DataLoader
 from pathlib import Path
 
@@ -61,20 +61,31 @@ def main():
     if args.load_from_checkpoint and not check_checkpoint_valid(args.load_from_checkpoint):
         raise ValueError('Invalid checkpoint file path')
 
+    checkpoint_callback = ModelCheckpoint(
+        dirpath='checkpoints',
+        filename='lstm-{epoch:02d}-{train_loss:.2f}', # TODO: instead of epoch, include date
+        monitor='train_loss',
+        save_last=True,
+        save_top_k=3,
+        mode='min'
+    )
     trainer = Trainer(
         devices=config.get('DEVICES', 'auto'),
         accelerator=config.get('ACCELERATOR', 'auto'),
-        default_root_dir=config.get('ROOT_DIR', 'checkpoints')
+        default_root_dir=config.get('ROOT_DIR', 'checkpoints'),
+        callbacks=[checkpoint_callback]
     )
 
-    train_dataset = TimeSeriesData(
-                    seq_len=config.get('SEQUENCE_LENGTH', 5))
-    train_loader = DataLoader(train_dataset, shuffle=False, num_workers=9, persistent_workers=True)
+    data_module = TimeSeriesDataModule(
+        data_dir=config.get('DATA_DIR', 'data/processed'),
+        batch_size=config.get('BATCH_SIZE', 1),
+        seq_len=config.get('SEQUENCE_LENGTH', 5)
+    )
     
     if args.load_from_checkpoint:
-        trainer.fit(model=lstm, ckpt_path=args.load_from_checkpoint)
+        trainer.fit(model=lstm, ckpt_path=args.load_from_checkpoint, datamodule=data_module) # TODO: do we need to pass datamodule?
     else:
-        trainer.fit(model=lstm, train_dataloaders=train_loader)
+        trainer.fit(model=lstm, train_dataloaders=train_loader, datamodule=data_module)
 
 
 if __name__ == '__main__':
